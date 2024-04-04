0404e8e267b119e446f2df4a4513ed45fcd53123
bogoreh <bogoreh026@gmail.com>
2024-04-04 21:42:25 +0300

    first initial

---
+++ src/containers/about/index.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
+
+const personalDetails = [
+  {
+    label: "Name",
+    value: "Abdoulrazak Bogoreh",
+  },
+  {
+    label: "Address",
+    value: "Djibouti",
+  },
+  {
+    label: "Email",
+    value: "bogoreh026@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "+251938175359",
+  },
+];
+
+const jobSummary =
+  "Experienced and self-reliant Front End Developer with a solid track record of 5 years, fusing the realms of design and programming to create captivating and interactive user experiences. Committed to crafting efficient websites through meticulous development, proactive feature enhancement, and relentless debugging. Deeply passionate about aesthetics and UI design, striving to provide a comprehensive and polished approach to every project. As a Front End Developer, your proficiency in leveraging UX and UI principles and adhering to design guidelines will be the benchmark of your success. Emphasizing attention to detail, you possess the ability to translate design concepts into practical solutions for prospective employers.";
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Front End Developer</h3>
+            <p>{jobSummary}</p>
+          </Animate>
+
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInformationHeaderText">
+              Personal Information
+            </h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(600px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <div className="about__content__servicesWrapper__innerContent">
+              <div>
+                <FaDev size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiAndroid size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <FaDatabase size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiApple size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
