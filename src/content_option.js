4deb1a9124570206423732cf8f09897da53356d0
bogoreh <76124689+bogoreh@users.noreply.github.com>
2025-07-16 06:18:58 +0300

    first initial

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "Abdibogoreh";
+const meta = {
+    title: "Abdibogoreh",
+    description: "I’m Abdibogoreh _ Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Abdibogoreh",
+    animated: {
+        first: "I love coding",
+        second: "I code websites",
+        third: "I develop Web Developer",
+    },
+    description: "Full-Stack Developer & Flutter Developer & WordPress",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Talk about myself",
+    aboutme: "My diverse experience in computer science and full-stack web development has deepened my passion and expertise. Seeking a challenging role that aligns with my skills and aspirations, I bring a strong work ethic, dedication to thorough preparation, and a confident ability to deliver results on complex projects..",
+};
+const worktimeline = [{
+        jobtitle: "Web Development and Designing (remote) OasisInfobyte",
+        where: "Satya Niketan, New Delhi",
+        date: "2022",
+    },
+    {
+        jobtitle: "Web Developer (remote) LetsGrowMore",
+        where: "Jasra, Uttar Pradesh, India",
+        date: "2023",
+    },
+    {
+        jobtitle: "ReactJS Intern at Sync",
+        where: "India",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "NextJS",
+        value: 70,
+    },
+    {
+        name: "PHP",
+        value: 65,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Web Developer",
+        description: "Developed and maintained high-quality frontend components for web applications using HTML, CSS, JavaScript, and React.",
+    },
+    {
+        title: "Flutter Developer",
+        description: "Cross-Platform Development Expertise & Modern Tech Stack Implementation & Full Development LifeCycle Ownership.",
+    },
+    {
+        title: "Wordpress Developer",
+        description: "Custom Theme & Plugin Development & Mastery of Core Technologies & Performance, Security & Maintenance.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "abdirazak047@gmail.com",
+    YOUR_PHONE: "(251)3815359",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/bogoreh",
+    facebook: "https://web.facebook.com/abdibogoreh.omar",
+    linkedin: "https://www.linkedin.com/in/abdoulrazak-omar/",
+    twitter: "https://x.com/Abdibogoreh1",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
